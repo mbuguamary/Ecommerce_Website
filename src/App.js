e6d0083908cd2a54e3a703d66d9e71ed96d59da3
Mary Mbugua <mbuguamels@gmail.com>
2023-05-24 11:22:12 +0300

    adding the routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import AppFooter from './Components/AppFooter';
+import PageContent from './Components/PageContent';
+import AppHeader from './Components/AppHeader';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <PageContent />
+      <AppFooter />
     </div>
   );
 }
+
 
 export default App;

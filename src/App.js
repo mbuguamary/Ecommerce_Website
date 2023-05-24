31fa436228460539d4010ec18ea224a5f4babf99
Mary Mbugua <mbuguamels@gmail.com>
2023-05-24 12:08:35 +0300

    added browser router to enable routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import AppFooter from './Components/AppFooter';
 import PageContent from './Components/PageContent';
 import AppHeader from './Components/AppHeader';
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <AppHeader />
       <PageContent />
       <AppFooter />
+      </BrowserRouter>
     </div>
   );
 }

8ea8e775665d09833f4cb3fc16f89188e21a9663
Mary Mbugua <mbuguamels@gmail.com>
2023-05-30 09:53:48 +0300

    added functionality to shopping cart

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
 import '../App.css';
-import {Badge, Menu} from "antd"
+import {Badge, Menu, Table} from "antd"
 import {HomeFilled,ShoppingCartOutlined } from "@ant-design/icons"
 import {useNavigate } from 'react-router-dom';
-import Typography from 'antd/es/typography/Typography';
+import {Typography,Drawer} from 'antd';
+import { getCart } from "../API";
 const AppHeader = () => {
     const navigate=useNavigate();
     const onMenuClick =(item) =>{
@@ -71,15 +73,72 @@ const AppHeader = () => {
                 label: "Fragrances",
                 Key: "fragrances",
 
-            }
+            },
            ]} 
         />
        <Typography.Title>Aamir Store</Typography.Title> 
-          <Badge className='shoopingCartIcon' count={7} >
-          <ShoppingCartOutlined />                                
-          </Badge>
+          <AppCart />
         </div>
   );
-}
+ }
+  function AppCart(){
+    const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
+    const [checkoutDrawerOpen, setCheckoutDrawerOpen] = useState(false);
+
+    const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    getCart().then((res) => {
+      setCartItems(res.products);
+    });
+  }, []);
+    
+    return(
+    <div>
+     <Badge  onClick={() => {
+        setCartDrawerOpen(true);
+      }}
+      count={7}
+      className="shoppingCartIcon"
+    >
+    <ShoppingCartOutlined />                                
+    </Badge>
+    <Drawer open={cartDrawerOpen}
+        onClose={() => {
+            setCartDrawerOpen(false);
+        }}
+        title="Your Cart"
+        contentWrapperStyle={{ width: 500 }}>
+            <Table  columns={[
+                {
+                    title:"Title",
+                    dataIndex:"title"
+                },
+                {
+                    title:"Price",
+                    dataIndex:"price",
+                    render: (value) => {
+                        return <span>${value}</span>;
+                      },
+                },
+                {
+                    title:"Quantity",
+                    dataIndex:"quantity"
+                },
+                {
+                    title:"Total",
+                    dataIndex:"total",
+                    render: (value) => {
+                        return <span>${value}</span>;
+                      },
+                },
+            ]}
+
+            dataSource={cartItems}
+            />
+        </Drawer>
+    </div>
+    );
+  }
+
 
 export default AppHeader

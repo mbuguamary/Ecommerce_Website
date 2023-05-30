f5ba9f555e57f33c4cb23576ea90e227961f643b
Mary Mbugua <mbuguamels@gmail.com>
2023-05-30 15:40:08 +0300

    added the footer message

--- src/Components/AppFooter.js
+++ src/Components/AppFooter.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import '../App.css';
+import { Typography } from 'antd';
 const AppFooter = () => {
   return (
-    <div className='appFooter'>AppFooter</div>
+    <div className="appFooter">
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Privacy Policy
+      </Typography.Link>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Terms & Conditions
+      </Typography.Link>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Return Policy
+      </Typography.Link>
+      <Typography.Link href="tel:+123456789" target={"_blank"}>
+        +123456789
+      </Typography.Link>
+    </div>
   )
 }
 
